4cb2e9b08d9b447c928d1ea130fe77089eb5b295
Michael Adams <mtadams42@gmail.com>
2022-06-26 13:03:28 -0600

    Added loading code

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -2,39 +2,42 @@ import * as PIXI from 'pixi.js'
 import { FpsMeter } from './fps_meter';
 
 const app = new PIXI.Application({
+    autoStart: false,
     width: 800,
     height: 450,
     antialias: true
 });
-app.ticker.maxFPS = 30;
 
-const container = document.getElementById('game') || document.body;
-container.appendChild(app.view);
+app.loader
+    .add('logo', 'images/logo.png')
+    .load(async (_, resources) => {
+        const sprite = new PIXI.Sprite(resources.logo!.texture);
 
-let fpsMeter: FpsMeter;
-const sprite = PIXI.Sprite.from('images/logo.png');
+        // Sprite
+        sprite.anchor.set(0.5);
+        sprite.x = app.renderer.width / 2;
+        sprite.y = app.renderer.height / 2;
+        app.stage.addChild(sprite);
 
-window.onload = load;
+        const container = document.getElementById('game');
+        if (!container) {
+            throw new Error("game container doesn't exist. Exiting.");
+        }
 
-function load() {
-    create();
-}
+        // make extra sure the container is empty
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        container.appendChild(app.view);
 
-function create() {
-    /* Sprite */
-    sprite.anchor.set(0.5);
-    sprite.x = app.renderer.width / 2;
-    sprite.y = app.renderer.height / 2;
-    app.stage.addChild(sprite);
+        const fpsMeter = new FpsMeter(container);
 
-    /* FPS */
-    fpsMeter = new FpsMeter(container);
+        app.ticker.add((delta) => {
+            fpsMeter.tick(app.ticker.FPS);
+            sprite.rotation += 0.1 * delta;
+            app.renderer.render(app.stage);
+        });
 
-    app.ticker.add(update);
-}
-
-function update(time: number) {
-    fpsMeter.tick(app.ticker.FPS);
-    sprite.rotation += 0.01 * time;
-    app.renderer.render(app.stage);
-}
+        // only start once everything's ready
+        app.start();
+    });
